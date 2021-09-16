1e1832d8bb20d729a7394d9d3209dfbbe76e80d7
padmini v <padmini.v@perfios.com>
2021-09-16 10:11:41 +0530

    ADDED: findCheapestHotel method to return the cheapest hotel in the given range

--- src/main/java/HotelReservationSystem/HotelReservation.java
+++ src/main/java/HotelReservationSystem/HotelReservation.java
@@ -1,18 +1,44 @@
 package HotelReservationSystem;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
+
 
 public class HotelReservation 
 {
-	
+	List<Hotel> hotels=new ArrayList<Hotel>();
 	public boolean addHotel(String hotelName,int rate)
 	{
 		Scanner scanner=new Scanner(System.in);
-		List<Hotel> hotels=new ArrayList<Hotel>();
 		Hotel hotel=new Hotel("A",2000);
 		hotels.add(hotel);
 		return true;
+	}
+	
+	public Hotel findCheapestHotel(String startRange,String endRange)
+	{
+
+		DateFormat date=new SimpleDateFormat("dd-mm-yy",Locale.ENGLISH);
+		LocalDate startDate=LocalDate.parse(startRange);
+		LocalDate endDate=LocalDate.parse(endRange);
+		
+		Hotel cheapestHotel=hotels.stream()
+				.min((n1,n2)->n1.getRateForRegularCustomer()-n2.getRateForRegularCustomer())
+				.orElse(null);
+		
+		long noOfDaysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+		long cheapestRate=noOfDaysBetween* cheapestHotel.getRateForRegularCustomer();
+		System.out.println(cheapestRate);
+		return cheapestHotel;
+		
 	}
 }
