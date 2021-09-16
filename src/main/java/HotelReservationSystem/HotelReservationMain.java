2a346b79060906791caf3e3db0829cb522e8655f
padmini v <padmini.v@perfios.com>
2021-09-16 08:17:19 +0530

    ADDED: Welcome message in the main method

---
+++ src/main/java/HotelReservationSystem/HotelReservationMain.java
@@ -0,0 +1,12 @@
+package HotelReservationSystem;
+
+public class HotelReservationMain 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("***Welcome to Hotel Reservation System***");
+
+	}
+
+}
