df2bc9d32273f5511ae5391bbbf25050681a1de5
padmini v <padmini.v@perfios.com>
2021-09-17 21:47:09 +0530

    REFACTORED: added welcome message

--- src/main/java/HotelReservationSystem/HotelReservationMain.java
+++ src/main/java/HotelReservationSystem/HotelReservationMain.java
@@ -1,15 +1,9 @@
 package HotelReservationSystem;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class HotelReservationMain 
 {
-
 	public static void main(String[] args) 
 	{
 		System.out.println("***Welcome to Hotel Reservation System***");
-		
 	}
-
 }
